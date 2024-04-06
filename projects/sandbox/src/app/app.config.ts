7f2e09e5595c645674c0e5ecefff99de7dd3acd3
Kapil Kumar <kapilkumar0037@gmail.com>
2024-04-06 13:37:10 +0530

    rate limit config changed

--- projects/sandbox/src/app/app.config.ts
+++ projects/sandbox/src/app/app.config.ts
@@ -1,17 +1,29 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { SPECIFIC_ENDPOINT_RATE_LIMIT_CONFIG } from './constants/constants'
+import {
+  GLOBAL_RATE_LIMIT_CONFIG,
+  SPECIFIC_ENDPOINT_RATE_LIMIT_CONFIG,
+} from './constants/constants';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { RATE_LIMIT_CONFIG, RateLimitInterceptor } from '../../../ngx-rate-limiter/src/public-api';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { RATE_LIMIT_CONFIG, RateLimitInterceptor } from 'ngx-rate-limiter';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-  provideHttpClient(
-    withInterceptorsFromDi()
-  ),
-  { provide: HTTP_INTERCEPTORS, useClass: RateLimitInterceptor, multi: true },
-  { provide: RATE_LIMIT_CONFIG, useValue: { specificEndPointsConfig: SPECIFIC_ENDPOINT_RATE_LIMIT_CONFIG} }
-  ]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: RateLimitInterceptor, multi: true },
+    {
+      provide: RATE_LIMIT_CONFIG,
+      useValue: {
+        specificEndPointsConfig: SPECIFIC_ENDPOINT_RATE_LIMIT_CONFIG,
+        allEndPointsConfig: GLOBAL_RATE_LIMIT_CONFIG,
+      },
+    },
+  ],
 };
--- projects/sandbox/src/app/constants/constants.ts
+++ projects/sandbox/src/app/constants/constants.ts
@@ -1,9 +1,19 @@
-import { RateLimitProperties, SpecificEndPointConfig } from '../../../../ngx-rate-limiter'
+import {
+  RateLimitProperties,
+  SpecificEndPointConfig,
+} from '../../../../ngx-rate-limiter';
 export const SPECIFIC_ENDPOINT_RATE_LIMIT_CONFIG: SpecificEndPointConfig[] = [
-    { urlEndsWith: '/launches', config: { maxRequests: 5, blockDuration: 5000, intervalDuration: 10000 } }
-]
+  {
+    urlEndsWith: '/launches',
+    config: {
+      maxRequests: 5,
+      blockDuration: 5000,
+      intervalDuration: 10000,
+    },
+  },
+];
 export const GLOBAL_RATE_LIMIT_CONFIG: RateLimitProperties = {
-    maxRequests: 5,
-    intervalDuration: 10000,
-    blockDuration: 5000
-} 
+  maxRequests: 5,
+  intervalDuration: 10000,
+  blockDuration: 5000,
+};
